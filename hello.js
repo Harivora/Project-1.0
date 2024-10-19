3b209950826104f4362a61a62c6df7e4ab940670
Harivora <harivora95@gmail.com>
2024-10-19 09:09:14 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,15 @@
+let a = "Hello world...!!!";
+
+console.log(a);
+
+let name  = "Harikrishna Vora";
+let age = 20;
+
+console.log("Name :: ",name);
+console.log("Age :: ",age);
+
+let b = "Kishan Viradiya";
+let c = 19;
+
+console.log("Name :: ",b);
+console.log("Age :: ",c);
